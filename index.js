1e80c456148e73ebc3d6f9c966c141910be1dc1c
Jose <jose.antonio.umeres@gmail.com>
2022-08-01 20:34:21 -0500

    Added environment variables

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 
+const dotenv = require('dotenv')
+dotenv.config();
+
 // server
 const app = express();
 
+// Public directory
+// va la ruta de la carpeta public
+app.use(express.static('public'))
 // Routes
-app.get('/', (req, res) => {
-    res.json({
-        ok: true
-    })
-})
+// app.get('/', (req, res) => {
+//     res.json({
+//         ok: true
+//     })
+// })
+
 
 // listen to requests
-app.listen(3001, () => {
-    console.log(`Server running ${3001}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Server running ${process.env.PORT}`)
 });
