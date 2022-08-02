ef6d3ace8b8b734c7d22981b3dda6c7f93c463b6
Jose <jose.antonio.umeres@gmail.com>
2022-08-01 20:41:34 -0500

    Added auth route

--- index.js
+++ index.js
@@ -9,13 +9,9 @@ const app = express();
 // Public directory
 // va la ruta de la carpeta public
 app.use(express.static('public'))
-// Routes
-// app.get('/', (req, res) => {
-//     res.json({
-//         ok: true
-//     })
-// })
 
+// Routes
+app.use('/api/auth', require('./routes/auth'));
 
 // listen to requests
 app.listen(process.env.PORT, () => {
