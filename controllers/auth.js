206f914ba2029d9b4d019c4783d8671ddb23c677
Jose <jose.antonio.umeres@gmail.com>
2022-08-04 21:00:21 -0500

    Added a custom middleware to handle errors

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,8 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
+
 
 const createUser = (req, res = response) => {
     const { name, email, password } = req.body;
-
-    // handle errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        })
-    }
 
     res.status(201).json({
         ok: true,
@@ -23,14 +14,6 @@ const createUser = (req, res = response) => {
 }
 
 const loginUser = (req, res = response) => {
-
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        })
-    }
 
     const { email, password } = req.body;
 
--- routes/auth.js
+++ routes/auth.js
@@ -5,9 +5,10 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
-const router = Router();
-
 const { createUser, loginUser, renewToken } = require('../controllers/auth');
+const { validateFields } = require('../middlewares/validate-fields');
+
+const router = Router();
 
 router.post(
     '/new',
@@ -15,6 +16,7 @@ router.post(
         check('name', 'El nombre es obligatorio').not().isEmpty(),
         check('email', 'El email es obligatorio').isEmail(),
         check('password', 'La contraseña debe ser 6 caracteres').isLength({ min: 6 }),
+        validateFields
     ],
     createUser
 );
@@ -24,6 +26,7 @@ router.post(
     [
         check('email', 'El email es obligatorio').isEmail(),
         check('password', 'La contraseña debe ser 6 caracteres').isLength({ min: 6 }),
+        validateFields
     ],
     loginUser
 );
