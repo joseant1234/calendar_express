4efd81d686e4bab097713b8e12123cd54ac9a152
Jose <jose.antonio.umeres@gmail.com>
2022-08-08 08:13:41 -0500

    Updated renew token function

---
+++ middlewares/validate-jwt.js
@@ -0,0 +1,36 @@
+const { response } = require('express');
+const jwt = require('jsonwebtoken');
+
+const validateJWT = (req, res = response, next) => {
+    // es un estandar enviar headers personalizados con el prefijo x-
+    const token = req.header('x-token');
+    if (!token) {
+        return res.status(401).json({
+            ok: false,
+            msg: 'No hay token en la petición'
+        });
+    }
+
+    try {
+        // el verify retorna el payload con el que se firmo el token
+        const { uid, name } = jwt.verify(
+            token,
+            process.env.SECRET_JWT_SEED
+        );
+
+        req.uid = uid;
+        req.name = name;
+
+    } catch (error) {
+        return res.status(401).json({
+            ok: false,
+            msg: 'Token no válido'
+        })
+    }
+
+    next()
+}
+
+module.exports = {
+    validateJWT
+}
