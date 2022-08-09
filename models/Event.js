eea20dc9f711cf66e209e59868d110ddd1c29589
Jose <jose.antonio.umeres@gmail.com>
2022-08-09 09:04:52 -0500

    Saved an event

--- models/Event.js
+++ models/Event.js
@@ -18,8 +18,17 @@ const EventSchema = Schema({
     },
     user: {
         type: Schema.Types.ObjectId,
-        ref: 'User' //nombre del schema
+        ref: 'User', //nombre del schema
+        required: true
     }
 });
+
+// por defecto usa como serializador toJSON, se puede sobreescribir
+EventSchema.method('toJSON', function() {
+    // con this.toObject() es una referencia al objecto q se está serializando
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id
+    return object;
+})
 
 module.exports = model('Event', EventSchema);
