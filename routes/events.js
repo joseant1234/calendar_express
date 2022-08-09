d6327ee0b61805a0ef48f45cf9a129f62bdf215f
Jose <jose.antonio.umeres@gmail.com>
2022-08-09 08:54:38 -0500

    Added validation to the createEvent route

--- routes/events.js
+++ routes/events.js
@@ -3,15 +3,28 @@
     /api/events
 */
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { isDate } = require('../helpers/is-date');
+const { validateFields } =require('../middlewares/validate-fields');
 const { validateJWT } = require('../middlewares/validate-jwt');
 const { getEvents, createEvent, updateEvent, deleteEvent } = require('../controllers/events');
+
 const router = Router();
 // cualquier peticion debajo usará el middleware validateJWT
 router.use(validateJWT);
 
 router.get('/', getEvents);
 
-router.post('/', createEvent);
+router.post(
+    '/',
+    [
+        check('title', 'El título es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+        check('end', 'Fecha de finalización es obligatoria').custom(isDate),
+        validateFields,
+    ],
+    createEvent
+);
 
 router.put('/:id', updateEvent);
 
