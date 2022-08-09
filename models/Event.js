e0a950b7f611eeed69d9515266362260dc7bd3a9
Jose <jose.antonio.umeres@gmail.com>
2022-08-09 08:42:11 -0500

    Added event model

---
+++ models/Event.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const EventSchema = Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    notes: {
+        type: String
+    },
+    start: {
+        type: Date,
+        required: true
+    },
+    end: {
+        type: Date,
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User' //nombre del schema
+    }
+});
+
+module.exports = model('Event', EventSchema);
