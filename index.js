7d4b419c4a008c093e0f2931f4ae3bf20327002c
Jose <jose.antonio.umeres@gmail.com>
2022-08-14 11:03:50 -0500

    Added react calendarApp and the route to serve index.html

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
+// cualquier otra ruta tendrá q mostrar la página index.html
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+})
+
 // listen to requests
 app.listen(process.env.PORT, () => {
     console.log(`Server running ${process.env.PORT}`)
